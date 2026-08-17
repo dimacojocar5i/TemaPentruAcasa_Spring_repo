a8cca80193e4d6aed21a707771d5a939f5a00b1f
Cojocari Dumitru <dimacojocar5i@gmail.com>
2026-08-17 16:44:34 +0200

    Initial commit

---
+++ src/main/java/com/expentruacasa/exercitiudemo/ExercitiudemoApplication.java
@@ -0,0 +1,13 @@
+package com.expentruacasa.exercitiudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercitiudemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercitiudemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expentruacasa/exercitiudemo/ExercitiudemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expentruacasa.exercitiudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercitiudemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
